f54398fc76aca34498d5c53b1ce4611ec74f7e39
Vaibhav Raj Harsh <156331614+vaibhavrajharsh@users.noreply.github.com>
2026-05-09 22:21:13 +0530

    fix: correct import path for Context component

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import Context from "./Utils/Context.jsx";
+import Context from "./utils/Context.jsx";
 
 
 
